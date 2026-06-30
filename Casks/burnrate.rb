04e8245c1daab14d29abeb1037a5372bc371f1f9
Nonthawat K. <nontawatk@8interactive.co.th>
2026-06-30 09:06:00 +0700

    add rb file

---
+++ Casks/burnrate.rb
@@ -0,0 +1,11 @@
+cask "burnrate" do
+  version "0.1.0"
+  sha256 "5c244d29c115b483c341edf3390238ebfe7a09ef37bab2553d72ff1c864f5d9a"
+
+  url "https://github.com/kittayawat-aong/Burnrate/releases/download/v#{version}/Burnrate.zip"
+  name "Burnrate"
+  desc "Your app description here"
+  homepage "https://github.com/kittayawat-aong/Burnrate"
+
+  app "Burnrate.app"
+end
