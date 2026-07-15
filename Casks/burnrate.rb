6bd327630c53bfcd9919506a28de41c06966d8c1
Nonthawat K. <nontawatk@8interactive.co.th>
2026-07-15 11:44:25 +0700

    Update burnrate to 1.0.4

--- Casks/burnrate.rb
+++ Casks/burnrate.rb
@@ -1,6 +1,6 @@
 cask "burnrate" do
-  version "1.0.3"
-  sha256 "dd7df8a473f6b97d259dea58967c3f080585a096a8402e98e5aa24ea01a9072b"
+  version "1.0.4"
+  sha256 "a431f569a3c73f0e31bf3739b83ddf83c388adfb1f51bf4ca45a50f4c21f164e"
 
   url "https://github.com/kittayawat-aong/Burnrate/releases/download/v#{version}/Burnrate.zip"
   name "Burnrate"
